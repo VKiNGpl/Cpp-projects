a978ebbc3420c0620fd8b2151059f521daa1244a
Krystian Spiewak <krystianspiewak@gmail.com>
2019-07-22 20:23:24 -0500

    .

--- Revision/main.cpp
+++ Revision/main.cpp
@@ -9,23 +9,24 @@ int number, guess, tries=0;
 
 int main()
 {
-    cout<<"Welcome, I have just thought of a number between 1 and 100\n";
+    cout<<"Welcome, I have just thought of a number between 1 and 100.\n";
     srand(time(NULL));
     number=rand()%100+1;
 
     while(guess!=number)
     {
         tries++;
-        cout<<"Guess which number: \n";
-        cin>>guess;
+        cout<<"Guess which number: ";
+        cin>>guess; cout<<endl;
 
         if(guess==number)
             cout<<"That is the correct number! You win in "<<tries<<" attempts! \n";
         else if(guess<number)
-            cout<<"That is too low, try again!(This is the attempt number: "<<tries<<" ).\n";
+            cout<<"That is too low, try again!(This is the attempt number: "<<tries<<").\n";
         else if(guess>number)
-            cout<<"That is too high, try again!(This is the attempt number: "<<tries<<" ).\n";
+            cout<<"That is too high, try again!(This is the attempt number: "<<tries<<").\n";
     }
+
     getchar();getchar();
     system("pause");
 
