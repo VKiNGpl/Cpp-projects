5e872daaf1286cbc7115f74445fdeb4f388391b9
Krystian Spiewak <krystianspiewak@gmail.com>
2019-08-06 16:38:27 -0500

    8.6

--- Pasja informatyki/Pointers/main.cpp
+++ Pasja informatyki/Pointers/main.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
+int ile;
+
 int main()
 {
+    cout << "Ile liczb w tablicy: ";
+    cin >> ile;
+
+    int *tablica;               // wskaznik typu int o nazwie tablica
+    tablica = new int [ile];    // rezerwacja ilosci int'ow oraz okreslenie typu zarezerwowanej komorki
+
+    for(int i=0; i<ile; i++)
+    {
+        cout << int(tablica) << endl;
+        tablica ++;
+    }
+
+    delete [] tablica;
 
     return 0;
 }
