6ff8998f7f587a27fc412a74c5bec26f6d154ee7
Krystian Spiewak <krystianspiewak@gmail.com>
2019-08-14 00:14:24 -0500

    .

---
+++ SPOJ/Ciazowy specjalista/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+  {
+   unsigned N,X,Y,Z;
+   for(cin>>N;N--;cout<<(unsigned)(12*(Y-X/(Z-1.))+0.5)<<endl)
+        cin>>X>>Y>>Z;
+   return 0;
+  }
+
+/*#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    float X, Y, Z;
+    int D, wiek;
+
+    cin >> D;
+    for (int i=0; i<D; i++)
+    {
+    cin >> X >> Y >> Z;
+    wiek=abs((X-Z*Y+Y)/(Z-1)*12);
+    cout << round(wiek) << endl;
+    }
+    return 0;
+}
+*/
