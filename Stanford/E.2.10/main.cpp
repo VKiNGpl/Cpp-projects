4b6d257bbc752e010e7dd95d8c9089e1d4c76380
Krystian Spiewak <krystianspiewak@gmail.com>
2019-09-06 16:32:56 -0500

    9.6b

--- Stanford/E.2.10/main.cpp
+++ Stanford/E.2.10/main.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
-void askForElements(double []);
+void askForElements(double [], int);
+void findLowHigh(int&, int&);
 
 int main()
 {
-    int howMany = 1;
+    int howMany = 0;
     double *dArray;
     dArray = new double [howMany];
 
-    cout << *dArray;
+    askForElements(dArray, howMany);
+
+    cout << sizeof dArray;
 
     delete [] dArray;
 
     return 0;
 }
-void askForElements(double [])
+void askForElements(double arr[], int nCount)
 {
+    double dNum;
 
+    cout << "Enter the elements of the array, one per line." << endl
+         << "Use -1 to signal the end of the list." << endl;
+        while (dNum != -1)
+        {
+            arr[nCount-1] = dNum;
+            cout << " ? ";
+            cin >> dNum;
+            nCount++;
+        }
 }
