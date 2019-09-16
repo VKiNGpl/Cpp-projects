2d085b697384e6ddf94cf10e453b5f91fede03be
Krystian Spiewak <krystianspiewak@gmail.com>
2019-09-16 15:15:05 -0500

    9.16

--- Stanford/E.3.10/main.cpp
+++ Stanford/E.3.10/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
+    ifstream carroll;
+    ofstream buffer;
+    string sFilename;
+    char cLetter;
+
+    cout << "Input file: "; cin >> sFilename;
+    carroll.open(sFilename.c_str());
+    buffer.open("buffer.txt");
+
+    if (carroll.good()==false)
+    {
+        cout << "File does not exist." << endl;
+    }
+
+    while(cLetter != EOF)
+    {
+        cLetter = carroll.get();
+        if (cLetter != '<' && cLetter != EOF)
+        {
+        buffer << cLetter;
+        }
+    }
+
+    buffer.close();
+    carroll.close();
 
     return 0;
 }
