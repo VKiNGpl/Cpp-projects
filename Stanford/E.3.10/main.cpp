a691837ad0e5eff772bfa7d043cb495d68f12da3
Krystian Spiewak <krystianspiewak@gmail.com>
2019-09-16 23:42:59 -0500

    9.16

--- Stanford/E.3.10/main.cpp
+++ Stanford/E.3.10/main.cpp
@@ -1,17 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    ifstream carroll;
+    ifstream carroll, buff;
     ofstream buffer;
-    string sFilename;
-    char cLetter;
+    string sFilename, animal, pNoun, subst, veg, bdyOfWtr, pAnml, line;
+    size_t pos1, pos2, pos3, pos4, pos5, pos6;
 
-    cout << "Input file: "; cin >> sFilename;
+    cout << "Input file: ";
+    cin >> sFilename;
+    cin.ignore();
+    cout << "  animal: ";
+    getline(cin, animal);
+    cout << "  plural noun: ";
+    getline(cin, pNoun);
+    cout << "  sticky substance: ";
+    getline(cin, subst);
+    cout << "  plural vegetable name: ";
+    getline(cin, veg);
+    cout << "  body of water: ";
+    getline(cin, bdyOfWtr);
+    cout << "  plural animal name: ";
+    getline(cin, pAnml); cout << endl;
+
     carroll.open(sFilename.c_str());
     buffer.open("buffer.txt");
 
@@ -20,16 +36,50 @@ int main()
         cout << "File does not exist." << endl;
     }
 
-    while(cLetter != EOF)
+    while (carroll.good())
     {
-        cLetter = carroll.get();
-        if (cLetter != '<' && cLetter != EOF)
+        getline(carroll, line);
+        pos1 = line.find("<animal>");
+        pos2 = line.find("<plural noun>");
+        pos3 = line.find("<sticky substance>");
+        pos4 = line.find("<plural vegetable name>");
+        pos5 = line.find("<body of water>");
+        pos6 = line.find("<plural animal name>");
+        if(pos1!=string::npos)
         {
-        buffer << cLetter;
+            line.replace(pos1, 8, animal);
         }
+        if(pos2!=string::npos)
+        {
+            line.replace(pos2, 13, pNoun);
+            pos3 = line.find("<sticky substance>");
+        }
+        if(pos3!=string::npos)
+        {
+            line.replace(pos3, 18, subst);
+        }
+        if(pos4!=string::npos)
+        {
+            line.replace(pos4, 23, veg);
+        }
+        if(pos5!=string::npos)
+        {
+            line.replace(pos5, 15, bdyOfWtr);
+        }
+        if(pos6!=string::npos)
+        {
+            line.replace(pos6, 20, pAnml);
+        }
+        buffer << line << endl;
     }
-
     buffer.close();
+    buff.open("buffer.txt");
+    while (buff.good())
+    {
+        getline(buff, line);
+        cout <<  line << endl;
+    }
+    buff.close();
     carroll.close();
 
     return 0;
