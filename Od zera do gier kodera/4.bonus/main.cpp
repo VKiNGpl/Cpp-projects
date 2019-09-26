0db1af6d30748d2764ac180434da74e010fba1ed
Krystian Spiewak <krystianspiewak@gmail.com>
2019-09-25 21:40:17 -0500

    9.25

---
+++ Od zera do gier kodera/4.bonus/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+f()
+{
+    int k;
+    float i, j, r, x, y = -16;
+
+    while (puts(""), y++<15)
+        for (x=0; x++<79; putchar(" .:-;!/>)|&IH%*#"[k&15]))
+            for (i = k = r = 0; j = r*r-i*i-2+x / 25, i = 2*r*i+y / 10, j*j+i*i < 11 && k++ < 111; r = j);
+}
+
+
+int main()
+{
+    f();
+
+    return 0;
+}
+
