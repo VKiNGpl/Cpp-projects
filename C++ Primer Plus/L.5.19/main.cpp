ed13cf8744b062a1f71204b8ab8b1126a19c1786
Krystian Spiewak <krystianspiewak@gmail.com>
2020-01-10 01:08:11 -0600

    1.10

---
+++ C++ Primer Plus/L.5.19/main.cpp
@@ -0,0 +1,19 @@
+// textin4.cbp -- reading chars with cin.get()
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ch;         // should be int, not char
+    int count = 0;
+
+    while ((ch = cin.get()) != EOF) // test for end-of-file
+    {
+        cout.put(char(ch));
+        ++count;
+    }
+    cout << endl << count << " characters read\n";
+
+    return 0;
+}
